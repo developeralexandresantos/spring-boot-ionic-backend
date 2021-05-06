8aba8799d593d321ac4d6754d8dde38e093947e8
Alexandre Aparecido dos Santos <skeeter.182@uninove.edu.br>
2021-05-06 12:41:02 -0300

    Testando a primeira classe de domínio - Categoria

--- src/main/java/br/com/developeralexandresantos/cursomc/domain/Categoria.java
+++ src/main/java/br/com/developeralexandresantos/cursomc/domain/Categoria.java
@@ -1,2 +1,49 @@
-package br.com.developeralexandresantos.cursomc.domain;public class Categoria {
+package br.com.developeralexandresantos.cursomc.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Categoria implements Serializable {
+
+    private static final long serialVersionUID = 2183594983186033320L;
+
+    Integer id;
+    String nome;
+
+    public Categoria() {
+    }
+
+    public Categoria(Integer id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Categoria categoria = (Categoria) o;
+        return Objects.equals(id, categoria.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
