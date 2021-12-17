49139491c0964434362d46f785846041f84c4a65
developeralexandresantos <skeeter.182@uninove.edu.br>
2021-12-17 02:53:05 -0300

    Busca de produtos por nome e categorias

---
+++ src/main/java/br/com/developeralexandresantos/cursomc/dto/ProdutoDTO.java
@@ -0,0 +1,46 @@
+package br.com.developeralexandresantos.cursomc.dto;
+
+import br.com.developeralexandresantos.cursomc.domain.Produto;
+
+import java.io.Serializable;
+
+public class ProdutoDTO implements Serializable {
+    private static final long serialVersionUID = -2547625356952824779L;
+
+    private Integer id;
+    private String nome;
+    private Double preco;
+
+    public ProdutoDTO() {
+
+    }
+
+    public ProdutoDTO(Produto obj) {
+        this.id = obj.getId();
+        this.nome = obj.getNome();
+        this.preco = obj.getPreco();
+    }
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+}
---
+++ src/main/java/br/com/developeralexandresantos/cursomc/services/ProdutoService.java
@@ -0,0 +1,37 @@
+package br.com.developeralexandresantos.cursomc.services;
+
+import br.com.developeralexandresantos.cursomc.domain.Categoria;
+import br.com.developeralexandresantos.cursomc.domain.Produto;
+import br.com.developeralexandresantos.cursomc.repositories.CategoriaRepository;
+import br.com.developeralexandresantos.cursomc.repositories.ProdutoRepository;
+import br.com.developeralexandresantos.cursomc.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProdutoService {
+
+    @Autowired
+    private ProdutoRepository repo;
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    public Produto find(Integer id) {
+        Optional<Produto> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado! Id: " + id + ", Tipo: " + Produto.class.getName()));
+    }
+
+    public Page<Produto> search(String nome, List<Integer> ids, Integer page, Integer linesPerPage, String orderBy, String direction) {
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        List<Categoria> categorias = categoriaRepository.findAllById(ids);
+        return repo.findDistinctByNomeContainingAndCategoriasIn(nome, categorias, pageRequest);
+    }
+}
